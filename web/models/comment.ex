f44adb757a23b53f716ad44196571400726a35c4
Mark Windholtz <mark@agiledna.com>
2016-07-09 17:10:08 -0400

    adds Comments model

---
+++ web/models/comment.ex
@@ -0,0 +1,24 @@
+defmodule Blog.Comment do
+  use Blog.Web, :model
+
+  schema "comments" do
+    field :body, :string
+    belongs_to :post, Blog.Post
+
+    timestamps
+  end
+
+  @required_fields ~w(body)
+  @optional_fields ~w()
+
+  @doc """
+  Creates a changeset based on the `model` and `params`.
+
+  If no params are provided, an invalid changeset is returned
+  with no validation performed.
+  """
+  def changeset(model, params \\ :empty) do
+    model
+    |> cast(params, @required_fields, @optional_fields)
+  end
+end
