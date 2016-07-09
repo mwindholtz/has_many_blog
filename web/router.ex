7866e75e0b07c4d3aebf77738c7289fd9058c4ee
Mark Windholtz <mark@agiledna.com>
2016-07-09 16:54:36 -0400

    adds posts

--- web/router.ex
+++ web/router.ex
@@ -16,7 +16,8 @@ defmodule Blog.Router do
   scope "/", Blog do
     pipe_through :browser # Use the default browser stack
 
-    get "/", PageController, :index
+     resources "/posts", PostController
+     get "/", PageController, :index
   end
 
   # Other scopes may use custom stacks.
